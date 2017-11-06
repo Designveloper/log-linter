67cf1eec347eb3e75b30745dd3ad5c0ced1bf76a
Son Le <sonlt@dgroup.co>
2017-11-06 15:33:16 +0000

    eslint

--- tests/lib/rules/log-context.js
+++ tests/lib/rules/log-context.js
@@ -2,51 +2,50 @@
  * @fileoverview must have context
  * @author Hung Vo
  */
-"use strict";
+
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/log-context"),
+const { RuleTester } = require('eslint');
 
-    RuleTester = require("eslint").RuleTester;
+const rule = require('../../../lib/rules/log-context');
 
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("log-context", rule, {
+const ruleTester = new RuleTester();
+ruleTester.run('log-context', rule, {
 
-    valid: [ {
-      code: `
-
+  valid: [{
+    code: `
       function isSignatureValid(request) {
-  Log.setContext({
-    filename: filename,
-    function: 'isSignatureValid'
-  });
+      Log.setContext({
+        filename: filename,
+        function: 'isSignatureValid'
+      });
 
-  Log.info('Request from fb'.h1);
-  Log.info(headers['x-hub-signature'], JSON.stringify(body, null, 2));
-  shaObj.setHMACKey(APP_SECRET, 'TEXT');
-  shaObj.update(jsonString);
-  return signatureHeader;
-}`
-    }
+      Log.info('Request from fb'.h1);
+      Log.info(headers['x-hub-signature'], JSON.stringify(body, null, 2));
+      shaObj.setHMACKey(APP_SECRET, 'TEXT');
+      shaObj.update(jsonString);
+      return signatureHeader;
+    }`,
+  },
 
-        // give me some code that won't trigger a warning
-    ],
+    // give me some code that won't trigger a warning
+  ],
 
-    invalid: [
-        {
-            code: "function() {Log.info('123')}",
-            errors: [{
-                message: "Missing log context",
-                type: "Log_context_missing"
-            }]
-        }
-    ]
+  invalid: [
+    {
+      code: "function() {Log.info('123')}",
+      errors: [{
+        message: 'Missing log context',
+        type: 'Log_context_missing',
+      }],
+    },
+  ],
 });
